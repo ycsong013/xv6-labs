69d174707c64b1cbfc86c9f9e5356d0cbb43a952
Ethan <ethan@Yinchuans-MacBook-Air.local>
2023-08-12 07:05:03 +0800

    Lab 10 Mmap

--- kernel/proc.c
+++ kernel/proc.c
@@ -141,6 +141,11 @@ found:
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
 
+    // Clear VMAs
+    for(int i=0;i<NVMA;i++) {
+      p->vmas[i].valid = 0;
+    }
+    
   return p;
 }
 
@@ -153,6 +158,12 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+    
+  for(int i = 0; i < NVMA; i++) {
+    struct vma *v = &p->vmas[i];
+    vmaunmap(p->pagetable, v->vastart, v->sz, v);
+  }
+    
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -301,6 +312,14 @@ fork(void)
       np->ofile[i] = filedup(p->ofile[i]);
   np->cwd = idup(p->cwd);
 
+  for(i = 0; i < NVMA; i++) {
+    struct vma *v = &p->vmas[i];
+    if(v->valid) {
+      np->vmas[i] = *v;
+      filedup(v->f);
+    }
+  }
+    
   safestrcpy(np->name, p->name, sizeof(p->name));
 
   pid = np->pid;
